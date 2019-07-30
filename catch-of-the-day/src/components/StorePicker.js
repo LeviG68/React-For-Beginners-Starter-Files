8f34f199bffd6ba0e14d587ee83e479128f9f9c2
levi <glendenning_levi@hotmail.com>
2019-07-30 14:56:17 -0600

    app.js inventory and order added

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
 
-class StorePicker extends React.Component{
-  render(){
-    return <p>i am the store picker</p>
+class StorePicker extends React.Component {
+  render() {
+    return ( <form className = "Store-selector" >
+      <h2> Please pick a Store! </h2> 
+      <input type = "text" required placeholder = 'Store Name'/>
+      <button type= "submit" > Visit Store -> </button> 
+      
+      </form>
+    );
   }
 }
 
